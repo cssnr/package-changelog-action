caa89b3f3b2b5324ca897b96950bbf20d1973eff
Shane <6071159+smashedr@users.noreply.github.com>
2025-09-25 17:19:15 -0700

    Fix

--- src/index.js
+++ src/index.js
@@ -8,8 +8,6 @@ const semverValid = require('semver/functions/valid')
 const { Base64 } = require('js-base64')
 const { markdownTable } = require('markdown-table')
 
-// test
-
 const maps = {
     col: {
         n: { align: 'l', col: 'Package&nbsp;Name' },
